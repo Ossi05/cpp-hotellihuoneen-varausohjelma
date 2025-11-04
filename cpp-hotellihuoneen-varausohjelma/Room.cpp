b80117445582091e14b8917642d02e5125914c29
Ossi05 <77546709+Ossi05@users.noreply.github.com>
2025-11-04 11:51:48 +0200

    Update Room

--- cpp-hotellihuoneen-varausohjelma/Room.cpp
+++ cpp-hotellihuoneen-varausohjelma/Room.cpp
@@ -1,7 +1,20 @@
 #include "Room.h"
 
+std::unordered_map<RoomType, double> Room::prices = {
+	{ RoomType::Single, 100.0 },
+	{ RoomType::Double, 200.0 },
+};
+
 Room::Room(int room_number, RoomType type, bool is_occupied)
-	: room_number{ room_number }, type{ type }, is_occupied{ is_occupied }
-{
-}
+	: room_number{ room_number }, type{ type }, is_occupied{ is_occupied }, price{Room::get_price(type)}
+{}
+
+double Room::get_price() const{ return this->price; }
+bool Room::get_is_occupied() const { return this->is_occupied; }
+void Room::set_is_occupied(bool is_occupied){ this->is_occupied = is_occupied; }
+int Room::get_room_number() const {	return this->room_number; }
+
+RoomType Room::get_room_type() const { return this->type; }
+double Room::get_price(RoomType type) { return prices.at(type); }
+
 
--- cpp-hotellihuoneen-varausohjelma/Room.h
+++ cpp-hotellihuoneen-varausohjelma/Room.h
@@ -1,17 +1,32 @@
 #pragma once
 #include <iostream>
+#include <unordered_map>
 
 enum class RoomType {
 	Single,
 	Double
 };
 
-struct Room {
+class Room {
+		
+private:
+	static std::unordered_map<RoomType, double> prices;
 
 	const int room_number;
 	const RoomType type;
+	double price;
 	bool is_occupied;
-
+public:
 	Room(int room_number, RoomType type, bool is_occupied = false);
+
+	int get_room_number() const;
+	RoomType get_room_type() const;
+	double get_price() const;
+	bool get_is_occupied() const;
+	void set_is_occupied(bool is_occupied);
+
+
+	static double get_price(RoomType type);
+
 };
 
