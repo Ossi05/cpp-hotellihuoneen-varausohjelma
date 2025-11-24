414cbcaf2c1c65e4587f60c9f9a8278d2c9a5172
Ossi <77546709+Ossi05@users.noreply.github.com>
2025-11-24 18:11:32 +0200

    Add random hotel room amount

--- cpp-hotellihuoneen-varausohjelma/main.cpp
+++ cpp-hotellihuoneen-varausohjelma/main.cpp
@@ -1,11 +1,26 @@
 #include "HotelApp.h"
 #include <clocale>
-
+#include "utils.h"
+#include <string>
 
 int main() {
 
+	std::string hotel_name{ "C++ Hotelli" };
+	std::string csv_file_name{ "testi.csv" }; // Tiedosto, johon varaukset tallennetaan
+	int min_rooms_amt{ 40 }; // Minimi m‰‰r‰ huoneita
+	int max_rooms_amt{ 300 }; // Maksimi m‰‰r‰ huoneita
+
+
+	int room_amt{ get_random_number(min_rooms_amt, max_rooms_amt) };
+	// Tehd‰‰n huonem‰‰r‰ jaolliseksi kymmenell‰
+	room_amt =
+		room_amt % 10 == 0 ?
+		room_amt :
+		room_amt - (room_amt % 10);
+
+
 	std::setlocale(LC_ALL, "fi_FI");
-	HotelApp app{ "C++ hotelli", 20, "testi.csv"};
+	HotelApp app{ hotel_name, room_amt, csv_file_name };
 	app.run();
 	return 0;
 }
